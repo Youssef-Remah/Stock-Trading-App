68d995bf5405a03bfd2217e818a49b898a5d16d9
Youssef-Remah <yremah8@gmail.com>
2024-11-15 18:46:38 +0200

    Add XML comments to IFinnhubService

--- ServiceContracts/IFinnhubService.cs
+++ ServiceContracts/IFinnhubService.cs
@@ -1,9 +1,23 @@
 ﻿namespace ServiceContracts
 {
+	/// <summary>
+	/// Represents a service that makes HTTP requests to finnhub.io
+	/// </summary>
 	public interface IFinnhubService
 	{
+		/// <summary>
+		/// Returns company details such as company name, company logo, country
+		/// </summary>
+		/// <param name="stockSymbol">Stock symbol to search for</param>
+		/// <returns>Returns a dictionary that contains company details</returns>
 		Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol);
 
+
+		/// <summary>
+		/// Returns stock price details such as current price, change in price, high/low price of the day
+		/// </summary>
+		/// <param name="stockSymbol">Stock symbol to search for</param>
+		/// <returns>Returns a dictionary that contains stock price details</returns>
 		Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol);
 	}
 }
