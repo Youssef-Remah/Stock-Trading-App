0c5b61d53f6cfbe4043e375a445e93922b8aefe7
Youssef-Remah <yremah8@gmail.com>
2024-11-21 11:45:45 +0200

    Add Orders view model to ~/Models

---
+++ StockTradingApp/Models/Orders.cs
@@ -0,0 +1,11 @@
+﻿using ServiceContracts.DTO;
+
+namespace StockTradingApp.Models
+{
+	public class Orders
+	{
+		List<BuyOrderResponse>? BuyOrders { get; set; }
+
+		List<SellOrderResponse>? SellOrders { get; set; }
+	}
+}
