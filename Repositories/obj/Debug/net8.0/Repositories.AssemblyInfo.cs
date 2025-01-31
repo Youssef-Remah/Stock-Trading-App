86f744797d82c5ecc4bd45f4fa06fdea9a0c428f
Youssef-Remah <yremah8@gmail.com>
2025-01-31 16:08:25 +0200

    Modified Database Connection String in appsettings.json

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174f6b6db300f65a940ebd2de539cc28ff821361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9680613c30bb5d7feaf87ccb832946d9877a3ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174f6b6db300f65a940ebd2de539cc28ff821361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9680613c30bb5d7feaf87ccb832946d9877a3ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
